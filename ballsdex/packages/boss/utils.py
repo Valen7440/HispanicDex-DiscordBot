343fc5db4a9eebe2fc701d5bec1c15312096ad9c
Valen <luchimillan374@gmail.com>
2025-08-02 00:33:24 -0300

    add boss effects and update to last ballsdex commit

---
+++ ballsdex/packages/boss/utils.py
@@ -0,0 +1,83 @@
+AUTONOMOUS_COMMUNITY: list[str] = [
+    "Community of Madrid",
+    "Castile and Leon",
+    "Andalusia",
+    "Ceuta",
+    "Melilla",
+    "Canary Islands",
+    "Murcia",
+    "Extremadura",
+    "Galicia",
+    "Asturias",
+    "Cantabria",
+    "Basque Country",
+    "Navarra",
+    "Castile-La Mancha",
+    "Valencian Community",
+    "Catalonia",
+    "Balearic Islands",
+    "Aragon",
+    "La Rioja"
+]
+
+LATAM: list[str] = [
+    # Sudamérica
+    "Argentina",
+    "Bolivia",
+    "Brazil",
+    "Chile",
+    "Colombia",
+    "Ecuador",
+    "Guyana",
+    "Paraguay",
+    "Peru",
+    "Suriname",
+    "Uruguay",
+    "Venezuela",
+    
+    # Centroamérica
+    "Belize",
+    "Costa Rica",
+    "El Salvador",
+    "Guatemala",
+    "Honduras",
+    "Nicaragua",
+    "Panama",
+    
+    # Caribe (países independientes)
+    "Antigua and Barbuda",
+    "Bahamas",
+    "Barbados",
+    "Cuba",
+    "Dominica",
+    "Dominican Republic",
+    "Grenada",
+    "Haiti",
+    "Jamaica",
+    "Saint Kitts and Nevis",
+    "Saint Lucia",
+    "Saint Vincent and the Grenadines",
+    "Trinidad and Tobago",
+    
+    # Territorios dependientes en América Latina y el Caribe
+    "Anguilla",  # Reino Unido
+    "Aruba",  # Países Bajos
+    "Bermuda",  # Reino Unido
+    "Bonaire",  # Países Bajos
+    "British Virgin Islands",  # Reino Unido
+    "Cayman Islands",  # Reino Unido
+    "Curaçao",  # Países Bajos
+    "Falkland Islands",  # Reino Unido
+    "French Guiana",  # Francia
+    "Guadeloupe",  # Francia
+    "Martinique",  # Francia
+    "Montserrat",  # Reino Unido
+    "Puerto Rico",  # Estados Unidos
+    "Saba",  # Países Bajos
+    "Saint Barthelemy",  # Francia
+    "Saint Martin",  # Francia
+    "Sint Eustatius",  # Países Bajos
+    "Sint Maarten",  # Países Bajos
+    "Turks and Caicos Islands",  # Reino Unido
+    "US Virgin Islands"  # Estados Unidos
+]
